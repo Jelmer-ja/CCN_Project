a40e3a40b56257b66cbc8160eb4b124dd8662219
L <laura_tigchelaar@msn.com>
2017-12-11 15:15:43 +0100

    changed cropping algorithm for Kaggle/Assira dataset

--- cropImages.py
+++ cropImages.py
@@ -1,19 +1,14 @@
 import cv2
 import os
 
-newfolder = 'C:/Users/Laura/Documents/TEMP/cats/images/cropped_catfaces/'
+newfolder = 'C:/Users/Laura/Documents/MASTER/Computational Cognitive Neuroscience/Final Project/cats/Cat/cropped_catfaces/'
 
-i = 0
-for file in os.listdir('C:/Users/Laura/Documents/TEMP/cats/images/catfaces'):
+for file in os.listdir('C:/Users/Laura/Documents/MASTER/Computational Cognitive Neuroscience/Final Project/cats/Cat/catfaces'):
     print file
-    i = i+1
-    newfilename = newfolder + 'cat_' + str(i) + '.jpg'
-    filepath = 'C:/Users/Laura/Documents/TEMP/cats/images/catfaces/' + file
+    newfilename = newfolder + file # new name is same as old name, so they can be compared!
+    filepath = 'C:/Users/Laura/Documents/MASTER/Computational Cognitive Neuroscience/Final Project/cats/Cat/catfaces/' + file
     img = cv2.imread(filepath)
     cropped_img = cv2.resize(img, (28,28))
-    if i < 10:
-        cv2.imshow('image', cropped_img)
-        cv2.waitKey(0)
     try:
         cv2.imwrite(newfilename, cropped_img)
     except:
