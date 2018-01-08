c74bc73ccf4fef93ca47d3d2b725754c16fd6f92
Jelmer-ja <jelmer-ja@hotmail.com>
2018-01-08 17:24:04 +0100

    push attempt

--- ANN.py
+++ ANN.py
@@ -3,6 +3,7 @@ import numpy as np
 import chainer.links as L
 from chainer import Link, Chain, ChainList, report
 
+"""
 class Generator(Chain):
     def __init__(self):
         super(Generator, self).__init__()
@@ -60,6 +61,68 @@ class Discriminator(Chain):
         h = F.leaky_relu(self.l5a(self.l5(h)))
         y = F.sigmoid(self.l6(h))
         return y
+"""
+def lindim(dims, scale, n):
+    d = map(lambda x: x // scale, dims)
+    d = reduce(lambda x, y: x * y, d)
+    return d * n
+
+def convdim(dims, scale, n):
+    return (n, dims[0] // scale, dims[1] // scale)
+
+class Generator(Chain):
+    def __init__(self, n_z, out_shape):
+        super(Generator, self).__init__(
+            fc0=L.Linear(n_z, lindim(out_shape, 2**4, 256)),
+            dc1=L.Deconvolution2D(256, 128, 4, stride=2, pad=1),
+            dc2=L.Deconvolution2D(128, 64, 4, stride=2, pad=1),
+            dc3=L.Deconvolution2D(64, 32, 4, stride=2, pad=1),
+            dc4=L.Deconvolution2D(32, 1, 4, stride=2, pad=1),
+            bn0=L.BatchNormalization(lindim(out_shape, 2**4, 256)),
+            bn1=L.BatchNormalization(128),
+            bn2=L.BatchNormalization(64),
+            bn3=L.BatchNormalization(32)
+        )
+        self.out_shape = out_shape
+
+    def __call__(self, z, test=False):
+        h = F.relu(self.bn0(self.fc0(z)))
+        h = F.reshape(h, ((z.shape[0],) + convdim(self.out_shape, 2**4, 256)))
+        h = F.relu(self.bn1(self.dc1(h)))
+        h = F.relu(self.bn2(self.dc2(h)))
+        h = F.relu(self.bn3(self.dc3(h)))
+        h = F.sigmoid(self.dc4(h))
+        return h
+
+class Discriminator(Chain):
+    def __init__(self, in_shape):
+        super(Discriminator, self).__init__(
+            c0=L.Convolution2D(1, 32, 4, stride=2, pad=1),
+            c1=L.Convolution2D(32, 64, 4, stride=2, pad=1),
+            c2=L.Convolution2D(64, 128, 4, stride=2, pad=1),
+            c3=L.Convolution2D(128, 256, 4, stride=2, pad=1),
+            fc4=L.Linear(lindim(in_shape, 2**4, 256), 512),
+            #mbd=MinibatchDiscrimination(512, 32, 8),
+            fc5=L.Linear(512, 512+32),  # Alternative to minibatch discrimination
+            fc6=L.Linear(512+32, 2),
+            bn1=L.BatchNormalization(64),
+            bn2=L.BatchNormalization(128),
+            bn3=L.BatchNormalization(256)
+        )
+
+    def __call__(self, x, minibatch_discrimination=True, test=False):
+        h = F.leaky_relu(self.c0(x))
+        h = F.leaky_relu(self.bn1(self.c1(h)))
+        h = F.leaky_relu(self.bn2(self.c2(h)))
+        h = F.leaky_relu(self.bn3(self.c3(h)))
+        h = self.fc4(h)
+
+        #if minibatch_discrimination:
+            #h = self.mbd(h)
+        #else:
+        h = F.leaky_relu(self.fc5(h))
+        h = self.fc6(h)
+        return h
 
 class GeneratorDistribution(object):
     def __init__(self, range):
