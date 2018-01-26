3885e11025169c59541459c1d7d20e4ac1e6ef24
L <laura_tigchelaar@msn.com>
2018-01-26 17:18:01 +0100

    added larger images, removed catfaces from separate dataset

--- cropImages.py
+++ cropImages.py
@@ -1,14 +1,16 @@
 import cv2
 import os
 
-newfolder = 'C:/Users/Laura/Documents/MASTER/Computational Cognitive Neuroscience/Final Project/cats/Cat/cropped_catfaces/'
+newfolder = 'C:/Users/Laura/Documents/MASTER/Computational Cognitive Neuroscience/Final Project/cats/Cat/large_cropped_catfaces/'
 
-for file in os.listdir('C:/Users/Laura/Documents/MASTER/Computational Cognitive Neuroscience/Final Project/cats/Cat/catfaces'):
+curFolder = 'C:/Users/Laura/Documents/MASTER/Computational Cognitive Neuroscience/Final Project/cats/Cat/catfaces'
+
+for file in os.listdir(curFolder):
     print file
     newfilename = newfolder + file # new name is same as old name, so they can be compared!
-    filepath = 'C:/Users/Laura/Documents/MASTER/Computational Cognitive Neuroscience/Final Project/cats/Cat/catfaces/' + file
+    filepath = curFolder + '/' + file
     img = cv2.imread(filepath)
-    cropped_img = cv2.resize(img, (28,28))
+    cropped_img = cv2.resize(img, (100, 100))
     try:
         cv2.imwrite(newfilename, cropped_img)
     except:
